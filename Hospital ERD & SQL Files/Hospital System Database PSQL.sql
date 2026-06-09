dd2a9953d9266b9ef3e1d50b266a33fcd58ce87a
Rhys Foster <rhysfoster081@outlook.com>
2026-06-09 05:01:54 +0100

    Update: Staff Performance
    Two triggers created. One on the SHIFT entity, the other on FEEDBACK.
    
    Both triggers call the same function. This function automatically fills the STAFF_PERFORMANCE entity - depending on the data found in each table.
    
    E.g Staff member is late to work - calculated by using SHIFT data and then this is put through as a Negative performance.
    
    If negative or positive patient feedback in the FEEDBACK entity - is fed through to STAFF_PERFORMANCE as well.
    
    The STAFF_PERFORMANCE table will not only be a great way of monitoring STAFFs attitude towards their workplace, but also allow an analysis of how hospitals perform as a whole.

--- Hospital ERD & SQL Files/Hospital System Database PSQL.sql
+++ Hospital ERD & SQL Files/Hospital System Database PSQL.sql
@@ -386,15 +386,6 @@ CREATE OR REPLACE FUNCTION add_patient_illness()
 $$
 LANGUAGE PLPGSQL;
 
-CREATE TRIGGER trig_add_patient_illness
-
-	AFTER INSERT ON
-
-		appointment_result
-
-	FOR EACH ROW
-
-EXECUTE PROCEDURE add_patient_illness();
 
 CREATE OR REPLACE FUNCTION update_hospital_stock()
 
@@ -452,6 +443,72 @@ CREATE OR REPLACE FUNCTION update_hospital_stock()
 $$
 LANGUAGE PLPGSQL;
 
+
+CREATE OR REPLACE FUNCTION update_staff_performance()
+
+	RETURNS TRIGGER AS
+	$$
+
+	BEGIN
+
+		IF TG_TABLE_NAME = 'shift' THEN
+
+			IF
+				(new.clocked_in > new.shift_start)
+			THEN
+				INSERT INTO
+					staff_performance
+						(staff_id, performance_type, performance_desc) VALUES
+							(new.staff_id, 'Negative', CONCAT('Late to work by', ' ', EXTRACT(HOUR FROM(new.clocked_in - new.shift_start)), ' ', 'hours and', ' ', EXTRACT(MINUTE FROM(new.clocked_in - new.shift_start)), ' ', 'minutes'));
+
+			ELSEIF
+				(new.clocked_in < new.shift_start)
+			THEN
+				INSERT INTO
+					staff_performance
+						(staff_id, performance_type, performance_desc) VALUES
+							(new.staff_id, 'Positive', CONCAT('Early to work by', ' ', EXTRACT(HOUR FROM(new.shift_start - new.clocked_in)), ' ', 'hours and', ' ', EXTRACT(MINUTE FROM(new.shift_start - new.clocked_in)), ' ', 'minutes'));
+
+			ELSEIF
+				(new.clocked_out > new.shift_end)
+			THEN
+				INSERT INTO
+					staff_performance
+						(staff_id, performance_type, performance_desc) VALUES
+							(new.staff_id, 'Positive', CONCAT('Stayed an extra', ' ', EXTRACT(HOUR FROM(new.clocked_out - new.shift_end)), ' ', 'hours and', ' ', EXTRACT(MINUTE FROM(new.clocked_out - new.shift_end)), ' ', 'minutes', ' ', 'after shift ended'));
+
+			END IF;
+
+		ELSEIF TG_TABLE_NAME = 'feedback' THEN
+
+			IF 
+				(new.verified = TRUE)
+			THEN
+				IF
+					(new.feedback_type = 'Positive')
+				THEN
+					INSERT INTO
+						staff_performance
+							(staff_id, performance_type, performance_desc) VALUES
+								(new.staff_id, 'Positive', 'Patient left positive feedback about staff member');
+
+				ELSEIF
+					(new.feedback_type = 'Negative')
+				THEN
+					INSERT INTO
+							staff_performance
+								(staff_id, performance_type, performance_desc) VALUES
+									(new.staff_id, 'Negative', 'Patient left negative feedback about staff member');
+				END IF;
+			END IF;
+		END IF;
+		
+		RETURN NEW;
+	END;
+$$
+LANGUAGE PLPGSQL;
+
+
 CREATE TRIGGER trig_update_hospital_stock_appointment
 
 	AFTER INSERT ON
@@ -462,6 +519,7 @@ CREATE TRIGGER trig_update_hospital_stock_appointment
 
 EXECUTE PROCEDURE update_hospital_stock();
 
+
 CREATE TRIGGER trig_update_hospital_stock_prescription
 
 	AFTER INSERT ON
@@ -471,6 +529,39 @@ CREATE TRIGGER trig_update_hospital_stock_prescription
 	FOR EACH ROW
 
 EXECUTE PROCEDURE update_hospital_stock();
+
+
+CREATE TRIGGER trig_update_staff_performance_shift
+
+	AFTER INSERT ON
+
+		shift
+
+	FOR EACH ROW
+
+EXECUTE PROCEDURE update_staff_performance();
+
+
+CREATE TRIGGER trig_update_staff_performance_feedback
+
+	AFTER INSERT ON
+
+		feedback
+
+	FOR EACH ROW
+
+EXECUTE PROCEDURE update_staff_performance();
+
+
+CREATE TRIGGER trig_add_patient_illness
+
+	AFTER INSERT ON
+
+		appointment_result
+
+	FOR EACH ROW
+
+EXECUTE PROCEDURE add_patient_illness();
 
 
 -- **************************** VIEW / QUERY CREATION STARTS HERE **************************** --
